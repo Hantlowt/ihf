c28c7ad50e69a676f68be939f8872f8e366296c4
Hantlowt <lhote.antoine@gmail.com>
2020-10-25 23:36:45 +0100

    more simple example

--- example/example.py
+++ example/example.py
@@ -12,15 +12,13 @@ class Todo:
     def __init__(self, client):
         self.tasks = client.session.get_data('tasks', [])
         self.error = ''
-        self.client = client
 
-    async def add_task(self, name):
+    def add_task(self, name):
         if len(name) > 0:
             self.tasks += [Task(name)]
             self.error = ''
         else:
             self.error = 'Please enter a task'
-        await self.client.send_render()
 
     def delete_task(self, index):
         self.tasks.pop(index)
